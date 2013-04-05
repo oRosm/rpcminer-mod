67b4872690df48cf7a5087d38a26a2b4889e7905
Colin Mackie <cdmackie@gmail.com>
2013-04-04 17:13:10 -0700

    Add build solution for Visual Studio 2010.

--- src/rpcminer/rpcminerthreadgpu.cpp
+++ src/rpcminer/rpcminerthreadgpu.cpp
@@ -16,6 +16,8 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 **/
 
+#pragma once
+
 #if defined(_BITCOIN_MINER_CUDA_) || defined(_BITCOIN_MINER_OPENCL_)
 
 #include "rpcminerthreadgpu.h"
