5e07b94263d85a2b38e87b55a3117b285b95b821
Colin Mackie <cdmackie@gmail.com>
2013-04-05 09:53:04 -0700

    Add missing defined in cuda build.

--- buildcuda.bat
+++ buildcuda.bat
@@ -1,8 +1,13 @@
+copy /y "src\cuda\bitcoinminercuda.cu" "bin\bitcoinminercuda.cu"
+
 "%CUDA_PATH%bin\nvcc.exe" -ptx ^
 	--use-local-env --cl-version 2010 ^
 	-gencode=arch=compute_35,code=\"sm_35,compute_35\" -Xptxas=-v -Xopencc=-LIST:source=on ^
 	-m32 ^
+	-D_BITCOIN_MINER_CUDA_ ^
+	-DNVCC ^
 	-I"%CUDA_PATH%include" ^
 	-I"src\cuda" ^
-	-o "bin\bitcoinminercuda.ptx" "src\cuda\bitcoinminercuda.cu"
+	-o "bin\bitcoinminercuda.ptx" ^
+	"bin\bitcoinminercuda.cu"
 
