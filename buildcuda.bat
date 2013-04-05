0e59005b670e1fdad3f73e7a87cf526b29ab0e28
Colin Mackie <cdmackie@gmail.com>
2013-04-05 10:03:44 -0700

    Rebuilt release binaries.

--- buildcuda.bat
+++ buildcuda.bat
@@ -1,8 +1,12 @@
+copy /y "src\cuda\bitcoinminercuda.cu" "bin\bitcoinminercuda.cu"
+
 "%CUDA_PATH%bin\nvcc.exe" -ptx ^
 	--use-local-env --cl-version 2010 ^
 	-gencode=arch=compute_20,code=\"sm_20,compute_20\" ^
 	-m32 ^
+	-D_BITCOIN_MINER_CUDA_ ^
+	-DNVCC ^
 	-I"%CUDA_PATH%include" ^
 	-I"src\cuda" ^
-	-o "bin\bitcoinminercuda.ptx" "src\cuda\bitcoinminercuda.cu"
-
+	-o "bin\bitcoinminercuda.ptx" ^
+	"bin\bitcoinminercuda.cu"
